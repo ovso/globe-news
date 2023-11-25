66b2a08f1bff63160d5ab1ed3f2b4844773e45e9
jaeho.5 <ovsoce@gmail.com>
2023-11-25 20:56:10 +0900

    feat: WebActivity, WebScreen 구현

---
+++ app/src/main/java/io/github/ovso/globenews/feature/web/WebActivity.kt
@@ -0,0 +1,33 @@
+package io.github.ovso.globenews.feature.web
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.ovso.globenews.core.ui.theme.GlobenewsTheme
+import io.github.ovso.globenews.feature.home.HomeScreen
+import io.github.ovso.globenews.feature.home.HomeViewModel
+
+@AndroidEntryPoint
+class WebActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GlobenewsTheme {
+                // A surface container using the 'background' color from the theme
+                val viewModel: HomeViewModel = hiltViewModel()
+                val state by viewModel.uiState.collectAsState()
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    HomeScreen(state = state,)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/io/github/ovso/globenews/feature/web/WebScreen.kt
@@ -0,0 +1,47 @@
+package io.github.ovso.globenews.feature.web
+
+import android.app.Activity
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun WebScreen(
+    modifier: Modifier = Modifier,
+    url: String,
+) {
+    val context = LocalContext.current
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = {
+            WebView(it).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = WebViewClient()
+                loadUrl(url)
+            }
+        }, update = {
+            it.loadUrl(url)
+        }
+    )
+
+    BackHandler {
+        (context as? Activity)?.finish()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewWebScreen() {
+    WebScreen(url = "https://www.google.com")
+}
+
