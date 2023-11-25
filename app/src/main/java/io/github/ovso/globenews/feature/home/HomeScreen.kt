467bbd368ccebe6363fc599dc78f2786d99b2471
jaeho.5 <ovsoce@gmail.com>
2023-11-25 15:33:15 +0900

    feat: Card UI 적용

--- app/src/main/java/io/github/ovso/globenews/feature/home/HomeScreen.kt
+++ app/src/main/java/io/github/ovso/globenews/feature/home/HomeScreen.kt
@@ -1,17 +1,28 @@
 package io.github.ovso.globenews.feature.home
 
+import android.app.Activity
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.ovso.globenews.core.network.Article
@@ -21,6 +32,7 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     state: HomeUiState,
 ) {
+    val context = LocalContext.current
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = {
@@ -31,13 +43,20 @@ fun HomeScreen(
                 text = "Headlines",
                 style = MaterialTheme.typography.titleLarge
             )
-        }
-    ) {
-        when (state) {
-            is HomeUiState.Loading -> HomeLoading(modifier = Modifier.padding(it))
+        },
+        content = {
+            when (state) {
+                is HomeUiState.Loading ->
+                    HomeLoading(modifier = Modifier.padding(it))
 
-            is HomeUiState.Success -> HomeContent(modifier = Modifier.padding(it), state.articles)
+                is HomeUiState.Success ->
+                    HomeContent(modifier = Modifier.padding(it), state.articles)
+            }
         }
+    )
+
+    BackHandler {
+        (context as? Activity)?.finish()
     }
 }
 
@@ -53,16 +72,43 @@ private fun HomeLoading(modifier: Modifier = Modifier) {
 
 @Composable
 private fun HomeContent(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     articles: List<Article>,
 ) {
-    Column {
-        articles.forEach {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        items(articles) { article ->
+            ArticleCard(article)
+        }
+    }
+}
+
+@Composable
+private fun ArticleCard(article: Article) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 1.dp
+        ),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable {
+
+                },
+        ) {
             Text(
+                text = article.title.orEmpty(),
                 modifier = Modifier
-                    .fillMaxWidth(),
-                text = it.title.orEmpty(),
-                style = MaterialTheme.typography.bodyLarge
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
             )
         }
     }
